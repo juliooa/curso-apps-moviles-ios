a3a66fc8b867d591f04ffe19ea8c6da0b3a732b7
juliooa <julio.olivares.a@gmail.com>
2016-05-09 22:48:08 -0300

    Initial Commit

---
+++ SIGAapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SIGAapp
+//
+//  Created by Julio Olivares Alarcón on 5/9/16.
+//  Copyright © 2016 UTFSM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
