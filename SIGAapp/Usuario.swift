763bff29941da000e365ce7c6128068a4a709c3a
juliooa <julio.olivares.a@gmail.com>
2016-05-22 19:20:44 -0300

    first commit

---
+++ SIGAapp/Usuario.swift
@@ -0,0 +1,14 @@
+//
+//  holaMundo.swift
+//  SIGAapp
+//
+//  Created by Julio Olivares Alarcón on 5/9/16.
+//  Copyright © 2016 UTFSM. All rights reserved.
+//
+
+import Foundation
+
+class Usuario{
+    
+    var name: String = ""
+}
--- SIGAapp/ViewController.swift
+++ SIGAapp/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var emailBox: UITextField!
+    
+    @IBAction func login(sender: AnyObject) {
+        
+        let usuario = Usuario()
+        
+        usuario.name = emailBox.text!
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
